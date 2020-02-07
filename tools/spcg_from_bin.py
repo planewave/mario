0c9ce4e94e968695f8af045977acb9b2b1794fe1
Xiao Liu <xiao@skycope.com>
2020-02-07 15:31:37 -0800

    plot psd

--- tools/spcg_from_bin.py
+++ tools/spcg_from_bin.py
@@ -8,6 +8,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--rate', type=float, default=56e6, help='sampling rate in Hz')
+    parser.add_argument('--fc', type=float, default=0, help='centre frequency in Hz')
     parser.add_argument('file_path', type=str, help='path of the binary file')
     args = parser.parse_args()
 
@@ -16,11 +17,12 @@ def main():
         raw = np.fromfile(f, dtype=np.int16)
     data = raw[0::2] + 1j*raw[1::2]
 
-    _, ax = plt.subplots(figsize=(10,6))
-    ax.specgram(data, NFFT=4096, Fs=args.rate / 1e6, noverlap=2048,sides='twosided')
-    ax.set_xlabel('time (ms)')
-    ax.set_ylabel('frequency (MHz)')
-    ax.set_xticklabels(ax.get_xticks() / 1000)
+    _, ax = plt.subplots(2, 1, figsize=(10,6))
+    ax[0].specgram(data, NFFT=4096, Fs=args.rate / 1e6, Fc=args.fc / 1e6, noverlap=2048)
+    ax[0].set_xlabel('time (ms)')
+    ax[0].set_ylabel('frequency (MHz)')
+    ax[0].set_xticklabels(ax[0].get_xticks() / 1000)
+    ax[1].psd(data, NFFT=4096, Fs=args.rate, Fc=args.fc, noverlap=2048)
     plt.show()
 
 if __name__ == "__main__":
