3774d0e0eef746e5b11aad7df8192c543741cc69
Xiao Liu <xiao@skycope.com>
2020-04-21 15:17:01 -0700

    rename

--- utils/bin2spcg.py
+++ utils/bin2spcg.py
@@ -20,11 +20,11 @@ def main():
     power = 20 * np.log10(np.sqrt(np.mean((data * np.conj(data)).real)))
     print('signal power is {p:3.2f} dB'.format(p=power))
     _, ax = plt.subplots(2, 1, figsize=(10,6))
-    ax[0].specgram(data, NFFT=4096, Fs=args.rate / 1e6, Fc=args.fc / 1e6, noverlap=2048)
+    ax[0].specgram(data, NFFT=512, Fs=args.rate / 1e6, Fc=args.fc / 1e6)
     ax[0].set_xlabel('time (ms)')
     ax[0].set_ylabel('frequency (MHz)')
     ax[0].set_xticklabels(ax[0].get_xticks() / 1000)
-    ax[1].psd(data, NFFT=4096, Fs=args.rate, Fc=args.fc, noverlap=2048)
+    ax[1].psd(data, NFFT=1024, Fs=args.rate, Fc=args.fc)
     plt.show()
 
 
