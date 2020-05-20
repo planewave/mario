9f200758579e2aff89265afc6121e1bf5852b43e
planewave <xiao@skycope.com>
2020-05-19 17:59:40 -0700

    antenna idx in time domain plot

--- utils/visu_cap.py
+++ utils/visu_cap.py
@@ -14,7 +14,7 @@ usage: python visu_cap.py [folder path]
 """
 
 
-def visu_for_analysis(capture, fig_path, nfft=512, fs=56):
+def visu_for_analysis(capture, fig_path, nfft=512, fs=56, ant_idx=True):
     data = capture.payload
     _, ax = plt.subplots(3, 1, figsize=(8, 10))
     ax[0].specgram(data, NFFT=nfft, Fc=capture.header.fc_khz/1e3, Fs=fs,
@@ -22,21 +22,29 @@ def visu_for_analysis(capture, fig_path, nfft=512, fs=56):
     ax[0].set_xlabel('time (ms)')
     ax[0].set_ylabel('frequency (MHz)')
     ax[0].set_xticklabels(ax[0].get_xticks() / 1000)  # convert us to ms
-    ax[0].set_title('fc: {fc} MHz, fs: {fs} MHz, gain: {g} dB'.format(
+    ax[0].set_title('fc: {fc} MHz, fs: {fs} MHz, gain: {g} dB, SOM {som}'.format(
         fc=capture.header.fc_khz/1e3, fs=capture.header.fs_khz/1e3,
-        g=capture.header.gain_db))
+        g=capture.header.gain_db, som=capture.header.sensor_id))
 
-    ax[2].psd(data, NFFT=nfft, Fs=fs, noverlap=0)
+    ax[2].psd(data, NFFT=nfft, Fs=fs, Fc=capture.header.fc_khz/1e3, noverlap=0)
     ax[2].set_xlabel('frequency (MHz)')
 
     down_sample = 50
     data = np.abs(data[0:-1:down_sample])
     data[data < 1] = 1
     time = np.arange(len(data)) / 56e6 * down_sample * 1000
-    ax[1].plot(time, 20 * np.log10(data))
+    ax[1].plot(time, 20 * np.log10(data), color='steelblue')
     ax[1].set_ylim(bottom=20, top=80)
     ax[1].set_xlim(left=0, right=time[-1])
     ax[1].axhline(y=70, linestyle='--', color='r')
+
+    if ant_idx:
+        dwell_time = capture.header.ant_dwell_time_ms
+        for idx in range(capture.header.num_ant):
+            ax[1].axvline(x=(idx+1)*dwell_time, linestyle='--', color='k')
+            ant_seq = int(capture.header.ant_seq / (2 ** (4 * idx))) % 16
+            ax[1].text(x=idx*dwell_time+1, y=22S, s=str(ant_seq))
+
     ax[1].set_xlabel('time (ms)')
     ax[1].set_ylabel('power (dB)')
 
