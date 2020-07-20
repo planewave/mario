d5fe8b601bc33770d360e081b43429e1aacd81f3
planewave <xiao@skycope.com>
2020-07-20 16:11:55 -0700

    clean up two functions

--- utils/capture_quality.py
+++ utils/capture_quality.py
@@ -6,13 +6,32 @@ import numpy as np
 import argparse
 
 
-def detect_saturation(file_path, thr_satu=1600):
-    with open(file_path, 'rb') as f:
-        raw = np.fromfile(f, dtype=np.int16)
-    raw = np.abs(raw[52:-1:40])
-    satu = (np.sum(raw > thr_satu) / len(raw)) * 100
-    return satu
+def detect_saturation(data):
+    """
+    detect a capture (complex-value) is saturated
+    """
+    # decimation
+    decim_factor = 56
+    data = data[0: -1: decim_factor]
+    # max downsampling (max pooling)
+    window_size = 500
+    n_window = np.floor(data.size / window_size)
+    data_reshape = np.reshape(data[0: int(window_size * n_window)],
+                              [window_size, -1])
+    data_max = np.max(np.abs(data_reshape), axis=0)
+    # thr = 1600*1.41
+    return np.sum(data_max > 2262) / data_max.size
 
+
+def get_c42(x):
+    def moment(x, p, q):
+        return np.mean(x ** (p - q) * np.conj(x) ** q)
+    x -= np.mean(x)
+    c21 = moment(x, 2, 1).real
+    c42 = moment(x, 4, 2) - np.abs(moment(x, 2, 0)
+                                   ) ** 2 - 2 * moment(x, 2, 1) ** 2
+    c42 = c42.real / c21 ** 2
+    return c42
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
